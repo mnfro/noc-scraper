40c9ee322f3962ef832dfa027ce366b9e0ee7749
Manf <63849808+mnfro@users.noreply.github.com>
2023-09-18 23:35:18 +0200

    Update reset_flow.py

--- reset_flow.py
+++ reset_flow.py
@@ -4,7 +4,7 @@ import time
 def main():
    start_time = time.time()
    with open('./ignore_id.txt', 'w') as f:
-    f.write(json.dumps([]))
+      f.write(json.dumps([]))
    time_diff = time.time() - start_time
    print(f'Reset time: %.5f seconds.' % time_diff)
 
