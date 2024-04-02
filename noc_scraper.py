b9e97868642c209f4f2632e2edb24e57a92a02d9
Manf <63849808+mnfro@users.noreply.github.com>
2024-04-02 18:57:06 +0200

    Update noc_scraper.py
    refined keywords

--- noc_scraper.py
+++ noc_scraper.py
@@ -50,7 +50,7 @@ TL_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 Match = namedtuple('Match', ['keywords', 'data_params'])
 fujifilm_cameras = Match(['X100', 'X-100', 'X-E4', 'XE4'], DATA_FUJI_CAMERA)
 ricoh_cameras = Match(['GR'], DATA_RICOH_CAMERA)
-yashica_cameras = Match(['T'], DATA_YASHICA_CAMERA)
+yashica_cameras = Match(['T4'], DATA_YASHICA_CAMERA)
 canon_cameras = Match(['G7X'], DATA_CANON_CAMERA)
 fujifilm_lens = Match(['27'], DATA_FUJI_LENS)
 
